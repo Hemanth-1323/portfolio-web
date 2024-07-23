17fc6a714f9d486d33a6cf1768785b89a2419fe8
Hemanth D <126432799+Hemanth-1323@users.noreply.github.com>
2024-07-23 08:47:05 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,6 +2,7 @@ import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/pic13.jpeg";
 
+
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
@@ -30,6 +31,14 @@ user interfaces and overall web experiences.
 ];
 
 export const PROJECTS = [
+    {
+    title: "React Quiz",
+    image: project1,
+    link: "https://react-quiz-ukmq.vercel.app/",
+    description:
+      "Designed and developed a dynamic React quiz app, leveraging a JSON server fake API for real-time data interactions. Implemented responsive UI components ensuring a seamless user experience.",
+    technologies: ["React.js", "CSS", "HTML"],
+  },
   {
     title: "Theme Park Website",
     image: project1,
