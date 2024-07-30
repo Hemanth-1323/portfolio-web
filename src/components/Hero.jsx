a58fe97a6cf4c6222af063fe690fe7b9b25a55c4
Hemanth D <126432799+Hemanth-1323@users.noreply.github.com>
2024-07-30 19:19:10 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,8 +30,8 @@ const Hero = () => {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl"
             >
-              Front End Developer &
-              Machine Learning Enthusiast
+              Front End Developer & <span>
+              Machine Learning Enthusiast <span/>
             </motion.span>
             <motion.p
               variants={container(1)}
